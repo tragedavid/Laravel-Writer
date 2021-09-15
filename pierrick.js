6166bac7a8937a9009cc01acb90c67f21877936e
Gérémi Zanin <gzanin@entreloups.com>
2021-09-15 09:32:55 +0200

    first commit

---
+++ pierrick.js
@@ -0,0 +1,68 @@
+function generateRoute() {
+    document.getElementById('route').innerHTML += "Route::" + action.value + "('" + custom_url.value + "', '" + routeController.value 
+    + "@" +  method.value + "');<br>";
+    // custom_url.value + controller.value + method.value;
+}
+
+var isMigrationDefaultChecked = document.querySelector("input[name=default]");
+var isMigrationForeignChecked = document.querySelector("input[name=foreign]");
+var isOrderByChecked = document.querySelector("input[name=orderBy]");
+
+
+isMigrationDefaultChecked.addEventListener( 'change', function() {
+    if(this.checked) {
+        migration_default.style.display = "inline-block";
+    } else {
+        migration_default.style.display = "none";
+    }
+});
+isMigrationForeignChecked.addEventListener( 'change', function() {
+    if(this.checked) {
+        migration_foreign.style.display = "inline-block";
+    } else {
+        migration_foreign.style.display = "none";
+    }
+});
+isOrderByChecked.addEventListener( 'change', function() {
+    if(this.checked) {
+        order_by.style.display = "inline-block";
+    } else {
+        order_by.style.display = "none";
+    }
+});
+
+function generateMigration() {
+    document.getElementById('migration').innerHTML += "$table->" + type.value + "('" + column.value + "')";
+    if (document.querySelector('input[name="unsigned"]:checked') ) {
+        document.getElementById('migration').innerHTML += "->unsigned()";
+    }
+    if (document.querySelector('input[name="default"]:checked') ) {
+        // à voir selon les préférences de pierrick
+        defaultValue = default_value.value;
+        if (type.value === "string") {
+            defaultValue = "'" + default_value.value + "'";
+        }
+        document.getElementById('migration').innerHTML += "->default(" + defaultValue + ")";
+    }
+    if (document.querySelector('input[name="foreign"]:checked') ) {
+        document.getElementById('migration').innerHTML += ";<br><pre>$table->foreign('" + column.value + "')<br>   ->references('" + 
+        column_ref.value + "')<br>   ->on('" + table.value + "');</pre><br>";
+    }
+    else {
+        document.getElementById('migration').innerHTML += ";<br>";
+    }
+}
+
+function generateControllerGet() {
+
+    if (document.querySelector('input[name="orderBy"]:checked') ) {
+        document.getElementById('controller').innerHTML += "$" + variableGet.value + " = " + model_name.value + "::orderBy('" + 
+        orderByCol.value + "')" + "->get();<br>";
+    }
+    else {
+        document.getElementById('controller').innerHTML += "$" + variableGet.value + " = " + model_name.value + "::get();<br>";
+
+    }
+
+}
+
